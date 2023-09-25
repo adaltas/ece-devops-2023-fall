f7be70a477163aa7a2b901437c0687526287b657
gonzaloetjo <gonzaloetjo@gmail.com>
2023-09-25 11:36:00 +0200

    feat: all modules fix

--- modules/03.continuous-testing/lab-corrections/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog 
-
-## v1.1.0
-
-- Get user functionality
-- Create user functionality
-- Create web server
--- modules/03.continuous-testing/lab-corrections/src/configure.js
+++
@@ -1,7 +0,0 @@
-const mixme = require('mixme')
-const config_default = require('../conf/default')
-
-module.exports = (config_custom = {}) => {
-  const config = mixme.merge(config_default, config_custom)
-  return config
-}
